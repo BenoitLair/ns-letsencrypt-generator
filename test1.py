d464725ef9b92c1f84e4dc7dfe737dab92f7e4ca
Benoit Lair <kurushi9000@gmail.com>
2017-03-07 13:02:11 +0000

    commit from cloud9

---
+++ test1.py
@@ -0,0 +1,19 @@
+#
+#
+#   APPLICATION DE GENERATION DE REPOSITORY NS-LETSENCRYPT
+#   AUTHOR : LAIR BENOIT
+#   CREATION : 07/03/2017
+#
+#
+from shutil import copyfile
+import sys
+
+#source file
+print (sys.argv[0])
+src_repository=sys.argv[0]
+#dest file
+print (sys.argv[1])
+dest_repository=sys.argv[1]
+
+
+copyfile(src_repository, dest_repository)
