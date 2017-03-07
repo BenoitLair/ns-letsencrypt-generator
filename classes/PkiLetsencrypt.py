ed03754a9f051192dba8ce110a18bd15ce7e97c6
Benoit Lair <kurushi9000@gmail.com>
2017-03-07 14:31:56 +0000

    classe pkiletsencrypt v0.1 ok, utilisation classe ok 1

---
+++ classes/PkiLetsencrypt.py
@@ -0,0 +1,28 @@
+class Pki_letsencrypt:
+    '''Classe definissant la manipulation des applications permettant de generer des certificats letsencrypt'''
+    
+    def __init__(self):
+        """Par defaut, notre surface est vide"""
+        self.homedir = "/root"
+    
+    def get_homedir(self):
+        """Cette methode se charge de lire le homedir."""
+        print(self.homedir)
+    
+    def del_homedir(self):
+        """Cette methode permet d'effacer le homedir."""
+        self.homedir = ""
+        
+    def set_homedir(self,parametre):
+        """Cette methode permet de set le homedir."""
+        self.homedir = parametre
+        
+    def get_list_of_certificates_in_prod(self):
+        """Cette methode permet de retourner un tableau avec la liste des certificats"""
+        #on va get la liste des cronjobs pour identifier les certificats en prod.
+
+def ecrire(self, message_a_ecrire):
+        """Methode permettant d'ecrire"""
+        #if self.surface != "":
+        #    self.surface += "\n"
+        #self.surface += message_a_ecrire
--- test1.py
+++ test1.py
@@ -5,16 +5,44 @@
 #   CREATION : 07/03/2017
 #
 #
+#try:
+#    import pymysql
+#    pymysql.install_as_MySQLdb()
+#except ImportError:
+#    pass
 from shutil import copytree
 import sys
 import os
+import sys
+import subprocess
+
+
 
 #source file
-print (sys.argv[1])
-src_repository=sys.argv[1]
-#dest file
 print (sys.argv[2])
-dest_repository=sys.argv[2]
+src_repository=sys.argv[2]
+#dest file
+print (sys.argv[3])
+dest_repository=sys.argv[3]
+
+#
+# Append location of Pki_letsencrypt.py to PYTHONPATH
+#
+print (sys.argv[1])
+try:
+        sys.path.append(sys.argv[1])
+except Exception as e:
+        print("Exception: "+str(e.args))
+
+#print(os.environ['PYTHONPATH'])
+print(sys.path)
+#from PkiLetsencrypt import Pki_letsencrypt
+
+try:
+        from PkiLetsencrypt import Pki_letsencrypt
+except:
+        print("Exception: Please provide correct directory path for pki_letsencrypt2 module")
+        sys.exit()
 
 
 #print(os.path.isdir("/home/el"))
@@ -23,8 +51,11 @@ dest_repository=sys.argv[2]
 #print "test2 : ";
 #print(os.path.exists(dest_repository))
 
+mon_pki_le = Pki_letsencrypt()
+mon_pki_le.get_homedir()
 
 if os.path.exists(src_repository) == True and os.path.exists(dest_repository) == False:
     #copyfile(src_repository, dest_repository)
     copytree(src_repository, dest_repository)
+    
 
