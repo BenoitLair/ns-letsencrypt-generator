36225ade67e05441a70b83f73cb9fe076c0992bc
Benoit Lair <kurushi9000@gmail.com>
2017-03-07 13:08:59 +0000

    commit from cloud9 4

--- test1.py
+++ test1.py
@@ -10,11 +10,11 @@ import sys
 
 #source file
 print (sys.argv[0])
-src_repository=sys.argv[0]
+#src_repository=sys.argv[0]
 #dest file
 print (sys.argv[1])
-dest_repository=sys.argv[1]
+#dest_repository=sys.argv[1]
 
 
 #copyfile(src_repository, dest_repository)
-copy(src_repository, dest_repository)
+#copy(src_repository, dest_repository)
