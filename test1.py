727845bec54acb53a5eea5004fe8127683a5663b
Benoit Lair <kurushi9000@gmail.com>
2017-03-07 13:27:02 +0000

    commit 6

--- test1.py
+++ test1.py
@@ -5,17 +5,26 @@
 #   CREATION : 07/03/2017
 #
 #
-from shutil import copy
+from shutil import copytree
 import sys
+import os
 
 #source file
 print (sys.argv[1])
-src_repository=sys.argv[0]
+src_repository=sys.argv[1]
 #dest file
 print (sys.argv[2])
-dest_repository=sys.argv[1]
+dest_repository=sys.argv[2]
 
 
+#print(os.path.isdir("/home/el"))
+#print "test1 : ";
+#print(os.path.exists(src_repository))
+#print "test2 : ";
+#print(os.path.exists(dest_repository))
 
-#copyfile(src_repository, dest_repository)
-#copy(src_repository, dest_repository)
+
+if os.path.exists(src_repository) == True and os.path.exists(dest_repository) == False:
+    #copyfile(src_repository, dest_repository)
+    copytree(src_repository, dest_repository)
+
