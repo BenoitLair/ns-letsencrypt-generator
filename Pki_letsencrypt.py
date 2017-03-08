31520d297d57dbbe97af075a6927360ffe0a98aa
Benoit Lair <kurushi9000@gmail.com>
2017-03-08 15:18:22 +0000

    ajout commentaires et readme

--- Pki_letsencrypt.py
+++ Pki_letsencrypt.py
@@ -1,9 +1,11 @@
+
+
 class Pki_letsencrypt:
     '''Classe definissant la manipulation des applications permettant de generer des certificats letsencrypt'''
     
-    def __init__(self):
+    def __init__(self,home_certs_dir):
         """Par defaut, notre surface est vide"""
-        self.homedir = "/root"
+        self.homedir = home_certs_dir
     
     def get_homedir(self):
         """Cette methode se charge de lire le homedir."""
@@ -20,9 +22,10 @@ class Pki_letsencrypt:
     def get_list_of_certificates_in_prod(self):
         """Cette methode permet de retourner un tableau avec la liste des certificats"""
         #on va get la liste des cronjobs pour identifier les certificats en prod.
+        
 
-def ecrire(self, message_a_ecrire):
-        """Methode permettant d'ecrire"""
-        #if self.surface != "":
-        #    self.surface += "\n"
-        #self.surface += message_a_ecrire
+#def ecrire(self, message_a_ecrire):
+        #"""Methode permettant d'ecrire"""
+        ##if self.surface != "":
+        ##    self.surface += "\n"
+        ##self.surface += message_a_ecrire
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ns-letsencrypt-generator
+
+Cette application sert à générer et a gérer des certificats ssl let's encrypt pour du netscaler
+L'application s'installe sur un serveur ubuntu 16.04
+
+Il faut installer : 
+
+pip install python-crontab
+
+pour que ns-letsencrypt fonctionnes il faut : 
+
+apt-get install python git python-pip curl
+pip install requests
