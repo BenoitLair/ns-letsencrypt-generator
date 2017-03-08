83d383a5c5f791a9efc5a8abca5e4bf93b002ed4
Benoit Lair <kurushi9000@gmail.com>
2017-03-08 16:45:44 +0000

    test dev1

--- Pki_letsencrypt.py
+++ Pki_letsencrypt.py
@@ -1,4 +1,8 @@
-
+from shutil import copytree
+import sys
+import os
+import sys
+import subprocess
 
 class Pki_letsencrypt:
     '''Classe definissant la manipulation des applications permettant de generer des certificats letsencrypt'''
@@ -23,7 +27,31 @@ class Pki_letsencrypt:
         """Cette methode permet de retourner un tableau avec la liste des certificats"""
         #on va get la liste des cronjobs pour identifier les certificats en prod.
         
-
+    def create_app_certificate(self,src_dir,dest_dir):
+        """Cette methode sert a crer une nouvelle entite de certificat"""
+        if os.path.exists(src_dir) == True and os.path.exists(dest_dir) == False:
+            #copyfile(src_repository, dest_repository)
+            print "creation nouveau directory"
+            copytree(src_dir, dest_dir)
+            return True
+        else:
+            return False
+        
+    def delete_app_certificate(self,force=False):
+        """Cette methode sert a supprimer une entite de certificat"""
+        if force == True:
+            print "delete_app_certificate:<force=true>.\n"
+            try:
+                os.rmdir(app_dir)
+            except OSError as ex:
+                print(ex)
+        elif force== False:
+            print "delete_app_certificate:<force=false>.\n"
+            
+        
+        
+        
+        
 #def ecrire(self, message_a_ecrire):
         #"""Methode permettant d'ecrire"""
         ##if self.surface != "":
--- test1.py
+++ test1.py
@@ -20,12 +20,13 @@ import subprocess
 
 
 
-#source file
-print (sys.argv[1])
-src_repository=sys.argv[1]
 #dest file
-print (sys.argv[2])
-dest_repository=sys.argv[2]
+print (sys.argv[1])
+dest_repository=sys.argv[1]
+#dest file
+#print (sys.argv[2])
+#dest_repository=sys.argv[2]
+
 
 #
 # Append location of Pki_letsencrypt.py to PYTHONPATH
@@ -40,11 +41,12 @@ dest_repository=sys.argv[2]
 #print(os.environ['PYTHONPATH'])
 #print(sys.path)
 #from PkiLetsencrypt import Pki_letsencrypt
+import Pki_letsencrypt
 
 try:
         from Pki_letsencrypt import Pki_letsencrypt
 except:
-        print("Exception: Please provide correct directory path for pki_letsencrypt2 module")
+        print("Exception: Please provide correct directory path for pki_letsencrypt3 module")
         sys.exit()
 
 #import python-crontab
@@ -55,10 +57,13 @@ except:
         sys.exit()
 # reste a importer python cronjobs pour gerer ls cronjobs de root.
 
+
+
 current_directory_pwd=os.getcwd()
 #set source ns-letsencrypt
-current_source=os.getcwd()+"/letsencrypt-src/ns-letsencrypt/"
-#print "current source : "+current_source
+current_src=os.getcwd()+"/letsencrypt-src/ns-letsencrypt/"
+current_dst=os.getcwd()+"/"+dest_repository
+print "current source : "+current_src
 
 #print(os.path.isdir("/home/el"))
 #print "test1 : ";
@@ -67,14 +72,24 @@ current_source=os.getcwd()+"/letsencrypt-src/ns-letsencrypt/"
 #print(os.path.exists(dest_repository))
 
 #print os.getcwd()
+
+# MAIN APPLICATION
 mon_pki_le = Pki_letsencrypt(current_directory_pwd)
+print "current src : "+current_src
+print "current dst : "+current_dst
+retour_create_certificate=mon_pki_le.create_app_certificate(current_src,current_dst)
+if retour_create_certificate == True:
+        print "retour cert create : True"
+else:
+        print "retour cert create : False"
 #mon_pki_le.set_homedir('/home')
 #mon_pki_le.get_homedir()
 #mon_pki_le.del_homedir()
 mon_pki_le.get_homedir()
 
-if os.path.exists(src_repository) == True and os.path.exists(dest_repository) == False:
+#if os.path.exists(src_repository) == True and os.path.exists(dest_repository) == False:
     #copyfile(src_repository, dest_repository)
-    copytree(src_repository, dest_repository)
+    #print "creation nouveau directory"
+    #copytree(src_repository, dest_repository)
     
 
